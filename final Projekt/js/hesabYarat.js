2951e8e23a03841ff4eb85243671563c06532b0c
MemmedzadeDilare <102686676+MemmedzadeDilare@users.noreply.github.com>
2022-04-24 23:20:49 +0400

    Add files via upload

--- final Projekt/js/hesabYarat.js
+++ final Projekt/js/hesabYarat.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
       $("#psifre").text("Boş qoymayın.Minimum 2,Maksimum 30 simvol ola biler")
     }
   })
+
   $("#ad").keyup(function(){
     if($("#ad").val() !=""){
       $("#ad").removeClass("is-invalid")
@@ -40,21 +41,50 @@ $(document).ready(function(){
     }
   })
   $("#istifadeciAdi").keyup(function(){
-    if($("#istifadeciAdi").val() !=""){
+    var nameval = $("#istifadeciAdi").val();
+    if(nameval !=""){
       $("#istifadeciAdi").removeClass("is-invalid")
       $("#istifadeciAdi").addClass("is-valid")
       $("#pistifadeciadi").text("Düzgündür")
       $("#pistifadeciadi").removeClass("text-danger");
       $("#pistifadeciadi").addClass("text-success");
     }
+    if (nameval.length < 2 || 30 < nameval.length) {
+      $("#istifadeciAdi").removeClass("is-valid");
+      $("#istifadeciAdi").addClass("is-invalid");
+      $("#pistifadeciadi").text("Minimum 2,Maksimum 30 simvol ola biler")
+      $("#pistifadeciadi").addClass("text-danger");
+
+  } else if (nameval.length >= 2 && 30 >= nameval.length) {
+      $("#istifadeciAdi").removeClass("is-invalid");
+      $("#istifadeciAdi").addClass("is-valid");
+      $("pistifadeciadi").text("Düzgündür")   
+      $("#pistifadeciadi").removeClass("text-danger");
+      $("#pistifadeciadi").addClass("text-success");   
+  }
   })
   $("#sifre").keyup(function(){
-    if($("#sifre").val() !=""){
+    var g = $("#sifre").val();
+    if(g !=""){
       $("#sifre").removeClass("is-invalid")
       $("#sifre").addClass("is-valid")
       $("#psifre").text("Düzgündür")
       $("#psifre").removeClass("text-danger");
       $("#psifre").addClass("text-success");
+    }
+    if (g.length < 2 || 30 < g.length) {
+        $("#sifre").removeClass("is-valid");
+        $("#sifre").addClass("is-invalid");
+        $("#psifre").text("Minimum 2,Maksimum 30 simvol ola biler")
+        $("#psifre").addClass("text-danger");
+
+    } else if (g.length >= 2 && 30 >= g.length) {
+        $(".signg").addClass("d-none");
+        $("#sifre").removeClass("is-invalid");
+        $("#sifre").addClass("is-valid");
+        $("#psifre").text("Düzgündür")   
+        $("#psifre").removeClass("text-danger");
+        $("#psifre").addClass("text-success");   
     }
   })
 //localSrorege elave etmek
@@ -63,11 +93,22 @@ $(document).ready(function(){
 }
 $("#hesabyarat").submit(function(e){
     e.preventDefault();
-    let num = Number(localStorage.getItem("ID")) + 1;
-    localStorage.setItem(`user${num}-name`, $("#ad").val());
-    localStorage.setItem(`user${num}-phone`, $("#telefon").val());
-    localStorage.setItem(`user${num}-username`, $("#istifadeciAdi").val());
-    localStorage.setItem(`user${num}-password`, $("#sifre").val());
-    localStorage.setItem("ID", num);
+    if($("#ad").val()==""&&$("#telefon").val()==""&&$("#istifadeciAdi").val()==""&&$("#sifre").val()==""){
+      $("#ugurluQeydiyat").addClass("d-none")
+      $("#ugursuzQeydiyat").removeClass("d-none")
+     }else if($("#ad").val()==""||$("#telefon").val()==""||$("#istifadeciAdi").val()==""||$("#sifre").val()==""){
+      $("#ugurluQeydiyat").addClass("d-none")
+      $("#ugursuzQeydiyat").removeClass("d-none")
+    }else{
+      let num = Number(localStorage.getItem("ID")) + 1;
+      localStorage.setItem(`user${num}-name`, $("#ad").val());
+      localStorage.setItem(`user${num}-phone`, $("#telefon").val());
+      localStorage.setItem(`user${num}-username`, $("#istifadeciAdi").val());
+      localStorage.setItem(`user${num}-password`, $("#sifre").val());
+      localStorage.setItem("ID", num);
+      $("#ugursuzQeydiyat").addClass("d-none")
+      $("#ugurluQeydiyat").removeClass("d-none")
+    }
+    
 });
 })
